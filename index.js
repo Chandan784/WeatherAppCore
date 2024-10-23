4c00bab2be2c34d070aa534f0c3164aeec2449d3
chandan <chandansamantaray784@gmail.com>
2024-10-23 22:18:29 +0530

    v2

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const api = {
 let searchBtn = document.querySelector("#search-icon");
 let cityValue = document.querySelector("#city-input").value;
 
+let temp = document.querySelector("#tempreature");
+let ws = document.querySelector("#windspeed");
+let humidity = document.querySelector("#humidity");
+let weatherIcon = document.querySelector("#wether-icon");
+
 async function getWeatherData() {
   let cityValue = document.querySelector("#city-input");
   console.log(cityValue);
@@ -19,7 +24,23 @@ async function getWeatherData() {
 
   console.log(data);
 
-  return data;
+  temp.innerHTML = ` ${data.main.temp}`;
+  humidity.innerHTML = ` ${data.main.humidity}`;
+  ws.innerHTML = ` ${data.wind.speed}`;
+
+  if (data.weather[data.weather.length - 1].main == "Clouds") {
+    weatherIcon.setAttribute(
+      "src",
+      "https://cdn-icons-png.flaticon.com/128/13169/13169712.png"
+    );
+  }
+
+  if (data.weather[data.weather.length - 1].main == "rains") {
+    weatherIcon.setAttribute(
+      "src",
+      "https://cdn-icons-png.flaticon.com/128/13169/13169712.png"
+    );
+  }
 }
 
 searchBtn.addEventListener("click", (e) => {
